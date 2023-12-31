33e53514aec5f4b2b16019a7ae41fb2dd0f22754
Habiba Ezzagrani <habibaezzagrani@MacBook-Air-de-Habiba.local>
2023-12-31 15:28:53 +0100

    first commit

--- spring-ai/src/main/java/Soring/AI/spring/ai/Repo/InfoRepo.java
+++
@@ -1,7 +0,0 @@
-package Soring.AI.spring.ai.Repo;
-
-import Soring.AI.spring.ai.Models.Infos;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InfoRepo extends MongoRepository<Infos,String> {
-}
